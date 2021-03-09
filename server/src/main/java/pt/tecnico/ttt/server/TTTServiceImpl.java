6673f58036029257a2656370cd430165b226fe2b
Miguel Pardal <miguel.pardal@tecnico.ulisboa.pt>
2021-03-09 00:34:36 +0000

    Move to new package

---
+++ server/src/main/java/pt/tecnico/ttt/server/TTTServiceImpl.java
@@ -0,0 +1,24 @@
+package pt.tecnico.ttt.server;
+
+import io.grpc.stub.StreamObserver;
+import pt.tecnico.ttt.*;
+
+public class TTTServiceImpl extends TTTGrpc.TTTImplBase {
+
+	/** Game implementation. */
+	private TTTGame ttt = new TTTGame();
+
+	@Override
+	public void currentBoard(CurrentBoardRequest request, StreamObserver<CurrentBoardResponse> responseObserver) {
+		// StreamObserver is used to represent the gRPC stream between the server and
+		// client in order to send the appropriate responses (or errors, if any occur).
+
+		CurrentBoardResponse response = CurrentBoardResponse.newBuilder().setBoard(ttt.toString()).build();
+
+		// Send a single response through the stream.
+		responseObserver.onNext(response);
+		// Notify the client that the operation has been completed.
+		responseObserver.onCompleted();
+	}
+
+}
